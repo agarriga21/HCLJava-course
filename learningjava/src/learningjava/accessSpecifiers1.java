5bad77fe251a7ef16b2345d147c0f70692ed4fd8
agarriga21 <78110107+agarriga21@users.noreply.github.com>
2021-01-27 15:38:06 -0600

    Add files via upload

---
+++ learningjava/src/learningjava/accessSpecifiers1.java
@@ -0,0 +1,12 @@
+package learningjava;
+
+public class accessSpecifiers1 {
+
+	public static void main(String[] args) {
+		//default
+		System.out.println("Dafault Access Specifier");
+		defAccessSpecifier obj = new defAccessSpecifier(); 		  
+        obj.display(); 
+
+	}
+}
